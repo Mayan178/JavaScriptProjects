34972d79715dcb65c780d5c6d5d88cedea4d8098
Marie Sz <salle_marie@hotmail.com>
2023-02-12 13:54:15 +0000

    Second commit

--- Javascript/Javascript_2.js
+++ Javascript/Javascript_2.js
@@ -2,6 +2,9 @@ function validateForm() {
 
     let fname = document.getElementById("firstname").value;
     let lname = document.getElementById("lastname").value;
+    // added in for challenge 30
+    let pnumber = document.getElementById("phonenumber").value;
+    // end of added in for challenge 30    
     let age = document.getElementById("age").value;
     var errorcount = 0;
     if (fname == "") {
@@ -14,10 +17,22 @@ function validateForm() {
         errorcount++;
     }
     else {document.getElementById("lastmessage").innerHTML = "";}
+    // added in for challenge 30
+    if (pnumber == "") {
+        document.getElementById("phonemessage").innerHTML = "Please enter your phone number *";
+        errorcount++;
+    }
+    else {document.getElementById("phonemessage").innerHTML = "";}
+    // end of added in for challenge 30
     if (age == "") {
         document.getElementById("agemessage").innerHTML = "Please select your age range *";
         errorcount++;
     }
     else {document.getElementById("agemessage").innerHTML = "";}
-    if (errorcount>0) {return false;}
+    if (errorcount>0) {
+        // added for challeng 30 to show alert message if any one form field is left empty
+        alert("Please, fill in the form where required");
+        // end of added for challeng 30 to show alert message if any one form field is left empty
+        return false;
+    }
   }
